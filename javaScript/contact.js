36993e572d5fd1778ec298508bc4a1d34fa06e37
franceswtt <107554148+franceswtt@users.noreply.github.com>
2022-11-10 12:49:05 +0800

    Updated Contact Me section design on 2022/11/10

--- javaScript/contact.js
+++ javaScript/contact.js
@@ -1,25 +1,3 @@
-//contact form
-
-const name = document.getElementById("yourName")
-const email = document.getElementById("yourEmail")
-const message = document.getElementById("yourMessage")
-
-function sendEmail(){
-    Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "franceswtt@gmail.com",
-        Password : "284F20D962ACE4B0539BAF574686BFDBEEBD",
-        To : 'franceswtt@gmail.com',
-        From : email.value,
-        Subject : "New message from my website",
-        Body : `Name: ${name} 
-        Email: ${email}
-        Body: ${message}`
-    }).then(
-      message => alert("Your message is well received.")
-    );
-}
-
 //contact info tabs
 
 const tabs = document.querySelector(".contact-info-tabs")
@@ -62,7 +40,3 @@ copyBtn.addEventListener("click", () => {
   }, 1500)
   
 })
-
-
-
-
